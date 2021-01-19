a747ad3f8da8a3059b3707a822e7897de6405e1b
2hoyeong <leehodud302@naver.com>
2021-01-20 00:27:26 +0900

    [fix] 현재 시간이어서 보는 기능이 제대로 동작하지 않던 점 수정
    유튜브 동영상의 시간을 이어서 보는 기능이 동작하지 않던 점 수정

--- scripts.js
+++ scripts.js
@@ -1,42 +1,47 @@
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
 function notYoutubeError() {
-    document.querySelector('#warning').innerHTML = "Only working on youtube";
+  document.querySelector('#warning').innerHTML = 'Only working on youtube';
 }
 
 var qrcode;
-chrome.tabs.executeScript({
-    code: '(' + function () {
+chrome.tabs.executeScript(
+  {
+    code:
+      '(' +
+      function () {
         return {
-            _url: window.location.href,
-            _time: document.querySelector('.ytp-time-current').innerText,
+          _url: window.location.href,
+          _time: document.querySelector('.ytp-time-current').innerText,
         };
-    } + ')()'
-}, function (results) {
-    if (results !== undefined) {
-        var result = results[0];
-        if (result) {
-            var url = new URL(result._url);
-            var videoId = getParameterByName("v", url);
-            var time = result._time.split(":")[0] * 60 + result._time.split(":")[1] * 1;
-            if (url.hostname === "www.youtube.com" && videoId !== null) {
-                qrcode.clear();
-                qrcode.makeCode("https://" + url.hostname.toString() + "/watch?v=" + videoId + "?t=" + time);
-                return;
-            }
+      } +
+      ')()',
+  },
+  function ([result]) {
+    if (result !== undefined) {
+      if (result) {
+        var url = new URL(result._url);
+        var videoId = getParameterByName('v', url);
+        var time = result._time.split(':')[0] * 60 + result._time.split(':')[1] * 1;
+        if (url.hostname === 'www.youtube.com' && videoId !== null) {
+          qrcode.clear();
+          qrcode.makeCode('https://' + url.hostname.toString() + '/watch?v=' + videoId + '&t=' + time);
+          return;
         }
+      }
     }
     notYoutubeError();
-});
+  },
+);
 
 window.onload = () => {
-    qrcode = new QRCode("qrcode");
+  qrcode = new QRCode('qrcode');
 };
